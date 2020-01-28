62c85b11312232f0cc5403c2b28e800a6705ed9d
arjitm786 <arjitm786@gmail.com>
2020-01-28 12:15:14 +0530

    modified:   src/app/urls/apisetting.ts

--- src/app/urls/apisetting.ts
+++ src/app/urls/apisetting.ts
@@ -7,6 +7,7 @@ export class apiSetting{
 // public static apiAdministration=Urls.backendUrl +":8083";
 // public static apiUser =Urls.backendUrl + ':8081';
 // public static apiInfrastructure =Urls.backendUrl + ':8084';
+//public static apiGateway =Urls.backendUrl +':8080';
 
 
 
@@ -14,10 +15,9 @@ public static apiAcademics =  Urls.backendUrl +"/dis/academics";
 public static apiAdministration=Urls.backendUrl +"/dis/administration";
 public static apiUser =Urls.backendUrl + '/dis/user';
 public static apiInfrastructure =Urls.backendUrl + '/dis/infrastructure';
-
+public static apiGateway =Urls.backendUrl;
 
 
 public static apieureka =Urls.backendUrl + ':8761';
-public static apiGateway =Urls.backendUrl +':8080';
 }
 
