a373f512440d1bd9cbaf4380fd08606de961e939
arjitm786 <arjitm786@gmail.com>
2020-02-14 16:25:14 +0530

    url

--- src/app/urls/urls.ts
+++ src/app/urls/urls.ts
@@ -1,8 +1,8 @@
 export class Urls{
     public  mainurl = "http://10.1.18.240:8080";
 
-   public static backendUrl = 'http://localhost'; 
-   public static frontendUrl = 'http://localhost:4200'; 
+   public static backendUrl = 'http://10.1.100.114'; 
+   public static frontendUrl = 'http://10.1.100.114:4200'; 
 
  // public static backendUrl = 'http://10.1.100.76:8080'; 
   //public static frontendUrl = 'http://10.1.100.76:4200'; 
