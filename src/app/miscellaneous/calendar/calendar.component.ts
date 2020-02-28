785625a7e9de36ad38d86e42fde8d73b5961c07b
Singhal-vishal <vishalsinghalcool16@gmail.com>
2020-02-28 12:28:17 +0530

    calendar events

--- src/app/miscellaneous/calendar/calendar.component.ts
+++ src/app/miscellaneous/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { TokenStorageService } from './../../authentication/token-storage.service';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Inject } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -7,6 +7,14 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 import bootstrapPlugin from '@fullcalendar/bootstrap';
 import { CalendarService } from './../../API_Service/calendar.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+
 
 @Component({
   selector: 'app-calendar',
@@ -16,10 +24,24 @@ import { CalendarService } from './../../API_Service/calendar.service';
 
 export class CalendarComponent {
 
-  constructor(private calendarService: CalendarService, private auth: TokenStorageService) {}
+  constructor(private calendarService: CalendarService, private auth: TokenStorageService, public dialog: MatDialog) {}
+  animal: string;
+  name: string;
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 
   @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
-
+  calendarModal : boolean = false;
   calendarVisible = true;
   calendarPlugins = [dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrapPlugin];
   calendarWeekends = true;
@@ -42,12 +64,33 @@ export class CalendarComponent {
 
   handleDateClick(arg) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.openDialog()
       this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
         title: 'New Event',
         start: arg.date,
         allDay: arg.allDay
       });
+     
     }
+  }
+
+  
+
+}
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
--- src/app/miscellaneous/miscellaneous.module.ts
+++ src/app/miscellaneous/miscellaneous.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarchartComponent } from './barchart/barchart.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarComponent, DialogOverviewExampleDialog } from './calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { PiechartComponent } from './piechart/piechart.component';
 import { HtmlDownloaderComponent } from './html-downloader/html-downloader.component';
+import { MatDialogModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    FullCalendarModule
+    FullCalendarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
   ],
-  declarations: [BarchartComponent, CalendarComponent, PiechartComponent, HtmlDownloaderComponent],
-  exports: [CalendarComponent, BarchartComponent, PiechartComponent, HtmlDownloaderComponent]
+  declarations: [BarchartComponent, CalendarComponent, PiechartComponent, HtmlDownloaderComponent,     DialogOverviewExampleDialog  ],
+  exports: [CalendarComponent, BarchartComponent, PiechartComponent, HtmlDownloaderComponent],
+  entryComponents: [DialogOverviewExampleDialog]
 })
 export class MiscellaneousModule { }
