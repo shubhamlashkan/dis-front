a02be4c15d37407397079db93a19a5d5d57e9268
RajnandiniRaghu <rajnandini.raghuvanshi001@gmail.com>
2020-03-02 10:45:07 +0530

    time table

--- src/app/myduties/timetable/sem-time-table/sem-time-table.component.ts
+++ src/app/myduties/timetable/sem-time-table/sem-time-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { checkList, TempList} from '../timetableModel';
+import { element } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-sem-time-table',
@@ -7,34 +9,96 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./sem-time-table.component.scss']
 })
 export class SemTimeTableComponent implements OnInit {
-  @ViewChild('f') facultyNameForm : NgForm;
-  @ViewChild('g') dayForm : NgForm;
-  @ViewChild('h') addTimetableForm : NgForm;
-  @ViewChild('i') facultyNameUpForm : NgForm;
+ valueslist : checkList[]=[];
+ xyzlist :TempList;
+  days:string[]; 
+ timeslot:string[];
+ checkedList:checkList[]=[];
+ rowCount:number;
+ columnCount:number;
+ valuesListSize:number;
+  // xyzlist = [
+  //   {
+  //     value1: 'monday',
+  //     value2: '10:00-11:00'
+  //   },
+  //   {
+  //     value1: 'tuesday',
+  //     value2: '10:00-11:00'
+  //   },
+  //   {
+  //     value1: 'wednesday',
+  //     value2: '10:00-11:00'
+  //   },
+  //   {
+  //     value1: 'thursday',
+  //     value2: '10:00-11:00'
+  //   },
+  //   {
+  //     value1: 'friday',
+  //     value2: '10:00-11:00'
+  //   },
+  // ];
 
-  showDay : boolean = false;
-  
 
-  constructor() { }
+
+
+
+
+
+  constructor() { 
+
+  }
+
 
   ngOnInit() {
-    this.showDay = false;
+    this.days = ['monday','tuesday','wednesday','thursday','friday'];
+    this.timeslot = ['10:00-11:00','11:00-12:00','12:00-1:00','2:00-3:00','3:00-4:00','4:00-5:00','5:00-6:00'];
+    this.columnCount = this.days.length;
+    this.rowCount = this.timeslot.length;
+    for(var i=0;i<this.rowCount;i++)
+    {
+      for(var j=0;j<this.columnCount;j++)
+      {
+          this.xyzlist = new TempList(this.timeslot[i],this.days[j]);
+          this.valueslist.push(this.xyzlist);
+        //  this.valueslist[this.valuesListSize].value2=this.timeslot[j];
+      }
+    }
+    //console.log(this.xyzlist[0].value1);
+    console.log(this.valueslist);
+    
+   // console.log(this.checkedList);
   }
-  onAddDay(){
-    this.showDay = true;
+  
+
+  onCheckboxChange(option, event) {
+    if(event.target.checked) {
+      this.checkedList.push(option);
+    } else {
+    for(var i=0 ; i < this.valueslist.length; i++) {
+      if(this.checkedList[i] == option) {
+        this.checkedList.splice(i,1);
+     }
+   }
+  }
+  console.log(this.checkedList);
   }
 
-  containers = [];
-
-  add() {
-    this.containers.push(this.containers.length);
-  }
-  remove()
+  uncheckAll()
   {
-    this.containers.pop();
-  }
-  onAddTimetable(){
+    this.valuesListSize = this.checkedList.length;
+    for(var i=0 ; i <this.valuesListSize; i++)
+     {
+             this.checkedList.pop();
+     } 
+     // console.log(this.checkedList);
+      
+    $('input[type="checkbox"]:checked').prop('checked',false);
+    
+      // element.checked= false;
+    }
 
-  }
- 
+    
 }
+
---
+++ src/app/myduties/timetable/timetableModel.ts
@@ -0,0 +1,15 @@
+export class checkList{
+value1 : string;
+value2:string;
+}
+
+
+export class TempList{
+     value1:string;
+     value2:string;
+    constructor($value1:string,$value2:string)
+    {
+        this.value1 = $value1;
+        this.value2 = $value2;
+    }
+}
