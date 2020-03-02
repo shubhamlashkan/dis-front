85290686e735e16592f40c87d186c9a6a3bbd9eb
RajnandiniRaghu <rajnandini.raghuvanshi001@gmail.com>
2020-03-02 13:10:23 +0530

    deleted:    src/app/myduties/timetable/semTimeTableModel.ts 	deleted:    src/app/myduties/timetable/semtimetable.service.spec.ts 	deleted:    src/app/myduties/timetable/semtimetable.service.ts

--- src/app/myduties/timetable/semTimeTableModel.ts
+++
@@ -1,9 +0,0 @@
-export interface SemTimeTableSettings
-{
-    id:string;
-    lecturelength:number;
-    lunchEndTime:string;
-    lunchStartTime:string;
-    modifiedBy:string;
-    modifiedDate:string;
-}
