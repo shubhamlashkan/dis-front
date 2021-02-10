ac61e3db0b7c2e7894317bf146777c66adeda7ed
shubham lashkan <shubhamlashkan@gmail.com>
2021-02-10 11:55:56 +0530

    modified:   src/app/hod/hod/hod.component.ts

--- src/app/hod/hod/hod.component.ts
+++ src/app/hod/hod/hod.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidenavService } from 'src/app/API_Service/sidenav.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-hod',
   templateUrl: './hod.component.html',
@@ -9,10 +9,19 @@ import { SidenavService } from 'src/app/API_Service/sidenav.service';
 })
 export class HodComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
-   
+    if(!this.isUserLoggedIn())
+    {
+      this.router.navigate(['login']);
+    }
   }
+  isUserLoggedIn()
+  {
+    let user = sessionStorage.getItem('authenticaterUser');
+    return !(user===null);
+  }
+
 
 }
