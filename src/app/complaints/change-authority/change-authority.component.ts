d78e4bec4c0c366e27799f40ccd7f7cae11e1685
Simran Makhija <simran.makhija2@gmail.com>
2021-11-17 15:26:27 +0530

    partially clean complaints module

--- src/app/complaints/change-authority/change-authority.component.ts
+++ src/app/complaints/change-authority/change-authority.component.ts
@@ -4,12 +4,6 @@ import { MatTableDataSource } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ToastrManager } from 'ng6-toastr-notifications';
 
-// export interface PeriodicElement {
-//   name: string;
-//   position: number;
-//   weight: number;
-//   symbol: string;
-// }
 
 export interface InfraInchargeDetails {
   id : string,
@@ -21,21 +15,8 @@ export interface InfraInchargeDetails {
 export interface NewInfraInchargeDetails {
   location : string,
   previousIncharge : string,
-//  selectNewIncharge : string,
-  //update : string;
 }
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// ];
+
 
 @Component({
   selector: 'app-change-authority',
@@ -101,8 +82,6 @@ export class ChangeAuthorityComponent implements OnInit {
           this.newInfraInchargeData.push({
             location :  this.infraInchargeData[i].location,
             previousIncharge : this.infraInchargeData[i].previousIncharge,
-           // selectNewIncharge : '',
-           // update : ''
           })
         }
         console.log(this.newInfraInchargeData);
