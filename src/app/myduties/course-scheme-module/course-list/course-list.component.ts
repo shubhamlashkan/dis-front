96b7cccd213db08f3a9e6f0e6669502ad4eadaae
Sonali Patel <sonalipatel23600@gmail.com>
2021-11-28 09:37:33 +0530

    add csv to html form

--- src/app/myduties/course-scheme-module/course-list/course-list.component.ts
+++ src/app/myduties/course-scheme-module/course-list/course-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonToCSVService } from 'src/app/API_Service/json-to-csv.service';
 import { CourseDetail } from 'src/app/Model/course-detail.model';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
 
 export interface SchemeElement {
   name: string;
@@ -36,6 +39,10 @@ COURSE_DATA.forEach(course => {
 })
 export class CourseListComponent implements OnInit {
   course: CourseDetail;
+  data: AOA = null;
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+
 
   constructor(private csvService : JsonToCSVService) { 
     this.course = new CourseDetail(null,null,null,0,0,0,0,0,0,0);
@@ -62,6 +69,40 @@ export class CourseListComponent implements OnInit {
 
   getCSV(){
     this.csvService.downloadFile(COURSE_DATA,"courses",header)
+  }
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 
   
--- src/app/myduties/course-scheme-module/external-list/external-list.component.ts
+++ src/app/myduties/course-scheme-module/external-list/external-list.component.ts
@@ -3,6 +3,10 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { JsonToCSVService } from 'src/app/API_Service/json-to-csv.service';
 import { CourseArray } from 'src/app/Model/course-detail.model';
 import { ExternalData } from 'src/app/Model/external-data.model';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
+
 export interface SchemeElement {
   name: string;
   institute: string;
@@ -40,6 +44,10 @@ export class ExternalListComponent implements OnInit {
   selectedCourses = new Set();
   courses = []
   coursesSettings:IDropdownSettings={};
+
+  data: AOA = null;
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
 
 
   constructor( private csvService : JsonToCSVService ) { 
@@ -96,5 +104,39 @@ export class ExternalListComponent implements OnInit {
 
   getCSV(){
     this.csvService.downloadFile(EXTERNAL_DATA,"external data",header)
+  }
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 }
